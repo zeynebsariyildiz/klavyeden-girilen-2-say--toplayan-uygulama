61aba4ebc908b8ab0f40f0d790face8e7cdd48bb
sefikanurtopuz <sefikanurtopuz@gmail.com>
2023-01-26 14:04:53 +0300

    kontrol edildi

--- Program.cs
+++ Program.cs
@@ -10,15 +10,15 @@ namespace klavyeden_girilen_2_sayıyı_toplayan_uygulama
     {
         static void Main(string[] args)
         {
-            int sayi1,sayi2,Toplam;
+            int sayi1,sayi2,Toplam; //bunu öğrenmen iyi olmuş :)))
             Console.WriteLine("Lütfen Bir Sayı Giriniz");
             sayi1=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Lütfen  Bir Sayı Daha Giriniz");
             sayi2 = Convert.ToInt32((Console.ReadLine()));
             Toplam = sayi1 + sayi2;
-            {
-                Console.WriteLine("Girdiğiniz Sayıların Toplamı = {0}",Toplam);
-            }
+            //{ //bu paranteze gerek yok
+            Console.WriteLine("Girdiğiniz Sayıların Toplamı = {0}",Toplam);
+            //} //bu paranteze gerek yok
             Console.ReadKey();
 
         }
